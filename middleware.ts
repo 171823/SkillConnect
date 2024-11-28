10bb23ab33dde73017341068c148ce9167fa4dbf
Niketan <159114184+niketan8928@users.noreply.github.com>
2024-11-28 22:47:50 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,27 @@
+// import { authMiddleware } from "@clerk/nextjs";
+
+// // This example protects all routes including api/trpc routes
+// // Please edit this to allow other routes to be public as needed.
+// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+// export default authMiddleware({
+//   publicRoutes: [
+//     "/",
+//     "/api/webhook/clerk",
+//     "/api/rapidapi",
+//     "/question/:id",
+//     "/tags",
+//     "/tags/:id",
+//     "/profile/:id",
+//     "/community",
+//     "/jobs",
+//   ],
+//   ignoredRoutes: ["/api/webhook/clerk", "/api/openai", "/api/rapidapi"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
 import { authMiddleware } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
@@ -14,10 +38,18 @@ export default authMiddleware({
     "/profile/:id",
     "/community",
     "/jobs",
+    "/welcome",
   ],
-  ignoredRoutes: ["/api/webhook/clerk", "/api/openai", "/api/rapidapi"],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/openai",
+    "/api/rapidapi",
+    //"/welcome",
+    "/api/todos",
+  ],
 });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
